bab4baa3c3027dc37ed8f15cd1c1e3e07c3e8417
Owen Johnson <60224822+OwenJohnsons@users.noreply.github.com>
2022-10-12 12:17:22 +0100

    Delete downsample.py

--- downsampling/downsample.py
+++
@@ -1,8 +0,0 @@
-import numpy as np 
-import blimpy 
-import argparse
-import re 
-
-parser = argparse.ArgumentParser(description='Downsamples the size of filterbanks')
-parser.add_argument('-i', '--inputfiles')
-
